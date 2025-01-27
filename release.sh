b9b1c47fc0f38a6ff6a41b038ddf3b9c8efedca7
Noah Kamara <mail@noahkamara.com>
2025-01-27 01:53:01 +0100

    chore: update release script

--- release.sh
+++ release.sh
@@ -1,14 +1,22 @@
 PUSH=false
+TAG=false
 
 for arg in "$@"; do
   if [ "$arg" = "--push" ]; then
+    TAG=true
     PUSH=true
+    break
+  fi
+
+  if [ "$arg" = "--tag" ]; then
+    TAG=true
     break
   fi
 
   if [ "$arg" = "--help" ]; then
     echo "./release.sh [--push]"
-    echo "  --push      push commit to repository"
+    echo "  --tag       create a tag locally"
+    echo "  --push      create tag push to repository"
     exit 0
   fi
 done
@@ -19,8 +27,9 @@ fi
 
 echo "VERSION is set to: $VERSION"
 
-printf "Building $VERSION"
+echo "Building $VERSION"
 ./build.sh || exit 1
+echo ""
 
 # Test Version
 TEST_VERSION="$(./urigami --version)"
@@ -32,21 +41,23 @@ else
   exit 1
 fi
 
-printf "Compressing"
-tar -czf "$VERSION.tar.gz" -C urigami
+echo "Compressing"
+tar -czf "$VERSION.tar.gz" urigami
 
-printf "Creating Tag"
-git tag -a "$VERSION" -m "Release $VERSION"
+if $TAG; then
+  echo "Creating Tag"
+  git tag -a "$VERSION" -m "Release $VERSION"
 
-if $PUSH; then
-  echo "Pushing Tag $VERION"
-  git push origin "$VERSION"
+  if $PUSH; then
+    echo "Pushing Tag $VERION"
+    git push origin "$VERSION"
 
-  cd $(brew --repo)/Library/Taps/noahkamara/homebrew-tap || exit 1
+    cd $(brew --repo)/Library/Taps/noahkamara/homebrew-tap || exit 1
 
-  ./release.sh urigami
+    ./release.sh urigami
 
-  git add Formula/urigami.rb
-  git commit -m "update(urigami): $VERSION"
-  git push origin
+    git add Formula/urigami.rb
+    git commit -m "update(urigami): $VERSION"
+    git push origin
+  fi
 fi
