dbf61d85dba80af2e480f896e9f0089c653c21ea
Noah Kamara <mail@noahkamara.com>
2025-01-27 01:46:26 +0100

    chore: update version

--- Sources/UrigamiCLI/UrigamiCLI.swift
+++ Sources/UrigamiCLI/UrigamiCLI.swift
@@ -14,7 +14,7 @@ struct UrigamiCLI: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "urigami",
         abstract: "a command line utility for getting or setting default apps",
-        version: "0.1.0",
+        version: "0.1.1",
         shouldDisplay: true,
         subcommands: [
             GetOpeningAppsCommand.self,
